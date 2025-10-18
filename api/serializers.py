b51cb265fdb5ff1004cd804d938d09ef3bcbbac2
Yash Pathak <ayushpathak7776@gmail.com>
2025-10-18 11:33:21 +0530

    @api_view(['GET','PUT','DELETE']) Request In Simpale Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,17 @@
 
 from rest_framework import serializers
-
+from . models import Movie
 
 class MovieSerializer(serializers.Serializer):
     name=serializers.CharField()
     descriptions=serializers.CharField()
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self,instance,validated_data):
+        instance.name=validated_data.get('name',instance.name)
+        instance.descriptions=validated_data.get('descriptions',instance.descriptions)
+        instance.save()
+        return instance
+    
--- api/views.py
+++ api/views.py
@@ -5,14 +5,43 @@ from .serializers import MovieSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view()
+@api_view(['GET','POST'])
 def all_movies(request):
-    movies=Movie.objects.all()
-    serializer=MovieSerializer(movies,many=True)
-    return Response(serializer.data)
+    if request.method=="GET":
+        movies=Movie.objects.all()
+        serializer=MovieSerializer(movies,many=True)
+        return Response(serializer.data)
+    
+    if request.method=="POST":
+        serializer=MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
-@api_view()
+
+
+@api_view(['GET','PUT','DELETE'])
 def movie(request,pk):
-    movie=Movie.objects.get(pk=pk)
-    serializer=MovieSerializer(movie)
-    return Response(serializer.data)
+
+    if request.method=="GET":
+        movie=Movie.objects.get(pk=pk)
+        serializer=MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    if request.method=="PUT":
+        movie=Movie.objects.get(pk=pk)
+        serializer=MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else :
+            return Response(serializer.errors)
+        
+    if request.method=="DELETE":
+        movie=Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response({'msg':"Data Deleted Suceefully"})
+
+
